f0155f55574f1ff7559b88d79b73c48262ef93d7
Stephen Muecke <smuecke@optusnet.com.au>
2017-03-20 16:23:06 +1030

    Add files via upload

---
+++ Sandtrap.Web/Models/ITableRow.cs
@@ -0,0 +1,16 @@
+﻿namespace Sandtrap.Web.Models
+{
+
+    public interface ITableRow
+    {
+        /// <summary>
+        /// Gets or sets a value indicating if the table row is marked for deletion.
+        /// </summary>
+        bool IsActive { get; set; }
+        /// <summary>
+        /// Gets of sets a value indicating if the values in the table row have changed.
+        /// </summary>
+        bool IsDirty { get; set; }
+    }
+
+}
