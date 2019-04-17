5d0d1dc80a2a6c69f15e85a64689c1dcaedbfa3e
Stephen Muecke <smuecke@optusnet.com.au>
2019-04-17 20:31:56 +0930

    Add files via upload

--- Sandtrap.Web/Scripts/sandtrap-table-v1.1.js
+++ Sandtrap.Web/Scripts/sandtrap-table-v1.1.js
@@ -24,10 +24,14 @@
 
         var self = this;
         // Declare the main UI components
-        this.form = $(this.element).closest('form');
-        var tableBodies = $(this.element).children('tbody');
-        this.body = tableBodies.first();
-        this.hiddenBody = tableBodies.last();
+        this.body = $(this.element).children('tbody').first();
+
+        // ***************************
+        //this.newRow = $(this.element).children('tbody').last().children('tr');
+        this.newRow = $(this.element).children('tbody').last();
+        // ***************************
+
+
         this.footer = $(this.element).children('tfoot').children('tr');
         this.addButton = this.footer.find('.add-button');
         // Set properties
@@ -38,20 +42,23 @@
         var data = $(this.element).data();
         this.isDirtyProperty = data['isdirtyproperty'];
         this.isActiveProperty = data['isactiveproperty'];
+        this.newRowIndex = this.body.children('tr').length / 2;
 
         // Ensure referenced plugins are present
-        if (this.hiddenBody.find('.numeric-input').length > 0) {
+        // TODO: Attach plugin for datepicker
+
+        if (this.newRow.find('.numeric-input').length > 0) {
             if ($.fn.numeric) {
                 // Attach plugins
                 this.body.find('.numeric-input').numeric({ rounding: this.options.rounding });
                 this.hasNumeric = true;
             } else {
                 // TODO: Load the plugin (from where?) or combine all into one file or bundle?
-                //console.error('The sandtrap-numeric plugin not loaded');
-                //return;
+                console.error('The sandtrap-numeric plugin not loaded');
+                return;
             }
         }
-        if (this.hiddenBody.find('.select-input').length > 0) {
+        if (this.newRow.find('.select-input').length > 0) {
             if ($.fn.selectlist) {
                 // Attach plugins
                 //this.body.find('.select-input').children('input[type="text"]').selectlist();
@@ -59,19 +66,39 @@
                 this.hasSelect = true;
             } else {
                 // TODO: Load the plugin (from where?) or combine all into one file or bundle?
-                //console.error('The sandtrap-select plugin not loaded');
-                //return;
+                console.error('The sandtrap-select plugin not loaded');
+                return;
             }
         }
-        if (this.hiddenBody.find('.datepicker-input').length > 0) {
+        if (this.newRow.find('.datepicker-input').length > 0) {
             if ($.fn.datepicker) {
                 // Attach plugins
                 this.body.find('.datepicker-input').datepicker();
                 this.hasDatepicker = true;
             } else {
                 // TODO: Load the plugin (from where?) or combine all into one file or bundle?
-                //console.error('The sandtrap-datepicker plugin not loaded');
-                //return;
+                console.error('The sandtrap-datepicker plugin not loaded');
+                return;
+            }
+        }
+        // Check for the IsActive and IsDirty inputs
+        var inputs = this.newRow.find('input');
+        if (this.isActiveProperty !== undefined) {
+            // Find the corresponding input
+            var attr = '[name="' + this.fieldName + '[#].' + this.isActiveProperty + '"]';
+            var isActiveInput = inputs.filter(attr);
+            if (isActiveInput.length !== 1) {
+                // No match found so prevent further searching
+                this.isActiveProperty = undefined;
+            }
+        }
+        if (this.isDirtyProperty !== undefined) {
+            // Find the corresponding input
+            var attr = '[name="' + this.fieldName + '[#].' + this.isDirtyProperty + '"]';
+            var isDirtyInput = inputs.filter(attr);
+            if (isDirtyInput.length !== 1) {
+                // No match found so prevent further searching
+                this.isDirtyProperty = undefined;
             }
         }
 
@@ -89,14 +116,30 @@
         });
 
         // Track changes
-        this.body.on('change', 'input, textarea, select', function () {
+        this.body.on('change itemSelected.select', 'input', function () {
+            // TODO: What about standard html select
             if (self.isDirtyProperty === undefined) {
                 return;
             }
+            // TODO: Sandtrap select uses hidden inputs which do not have a defaultValue
+            // Need to fix it and delete this
+            if ($(this).parent().is('.select-input')) {
+                var cell = $(this).closest('td');
+                var isDirtyInput = cell.find('input[Name$="' + self.isDirtyProperty + '"]');
+                isDirtyInput.val('True');
+                return;
+            }
+            // Find the corresponding inputs in both the cell and row
+            var cell = $(this).closest('td');
             var row = $(this).closest('tr');
-            var isDirtyInput = $(this).closest('tr').find('.' + self.isDirtyProperty);
+            var isDirtyInput = cell.find('input[Name$="' + self.isDirtyProperty + '"]');
+            if (isDirtyInput.length > 0) {
+                isDirtyInput.val($(this).val() !== $(this).prop('defaultValue'));
+            }
+            cell = row.children('td').last();
+            isDirtyInput = cell.find('input[Name$="' + self.isDirtyProperty + '"]');
             isDirtyInput.val(self.isDirty(row));
-        })
+        });
 
         // Update totals
         this.body.on('change', '.numeric-input', function () {
@@ -118,18 +161,6 @@
             //}
         });
 
-        // Update on submit
-        this.form.submit(function () {
-            self.hiddenBody.remove(); // Prevent the 'new row' being posted
-            // Mark any archived rows as dirty
-            if (self.isDirtyProperty && self.isActiveProperty) {
-                var rows = self.body.find('.archived');
-                $.each(rows, function (index, item) {
-                    $(this).find('.' + self.isDirtyProperty).val('True');
-                })
-            }
-        })
-
     }
 
     // **************Methods****************
@@ -144,14 +175,13 @@
             return;
         }
         // Calculate the totals of active inputs in the column
-        $.each(this.body.children('.edit-row'), function (index, row) {
+        $.each(this.body.children('tr'), function (index, row) {
             if (!$(row).hasClass('archived')) {
-                total += Number($(row).children('td').eq(colIndex).find('.numeric-input').val());
+                total += new Number($(row).children('td').eq(colIndex).find('.numeric-input').val());
             }
         });
         // Update the footer
-        // TODO: Add function or data-val attribute for formating the correct decimal places
-        //total = $(element).getFormattedValueFor(total);
+        total = $(element).getFormattedValueFor(total);
         //this.footer.children('td').eq(colIndex).text(total);
         footer.text(total);
     }
@@ -159,21 +189,29 @@
     // Adds a new row to the table
     table.prototype.addRow = function () {
         // Clone the new row
-        // TODO: Why doesn't this work to attach the plugins? - var clone = this.newRow.clone(true, true);
-        var clone = this.hiddenBody.clone();
-        // Update the indexers
-        var index = $.now();
-        clone.html($(clone).html().replace(/#/g, index));
+
+        // Why doesn't this work (attach the plugins)?
+        //var clone = this.newRow.clone(true, true);
+
+        var clone = this.newRow.clone();
+        // Update the index of the clone
+        clone.html($(clone).html().replace(/\[#\]/g, '[' + this.newRowIndex + ']'));
+        clone.html($(clone).html().replace(/"%"/g, '"' + this.newRowIndex + '"'));
+        this.newRowIndex++;
+
+        // Add the new row
+        this.body.append(clone.html());
+
         // Mark it as new
-        var newRows = clone.find('tr');
-        newRows.first().data('isnew', true);
-        // Add the new rows
-        this.body.append(newRows);
+        this.body.find('tr.edit-row').last().data('isnew', true);
+
         // Attach plugins
+        // TODO: Attach plugin for datepicker
         if (this.hasNumeric) {
             clone.find('.numeric-input').numeric({ rounding: this.options.rounding });
         }
         if (this.hasSelect) {
+            //clone.find('.select-input').children('input[type="text"]').selectlist();
             clone.find('.select-input').selectlist();
         }
         if (this.hasDatepicker) {
@@ -181,66 +219,70 @@
         }
         // Add validation
         if ($.validator) {
-            // TODO: Get the unobtrusive validation - this.form.data(unobtrusiveValidation);
-            // and add the rules for the new elements to save reparsing the whole form
-            // https://xhalent.wordpress.com/2011/01/24/applying-unobtrusive-validation-to-dynamic-content/
-            if ($.validator) {
-                this.form.data('validator', null);
-                $.validator.unobtrusive.parse(this.form)
-            }
+            // TODO: can we just add the validation to the clone without complete parse of the form?
+            var form = this.element.closest('form');
+            // Remove current form validation
+            form.removeData('validator').removeData('unobtrusiveValidation');
+            // Parse the form again
+            $.validator.unobtrusive.parse('form');
         }
-        // Set focus to the first input
-        newRows.find('.table-control').first().focus();
+        // Set focus to the first non hidden input
+        clone.find('input:not([type=hidden])').first().focus();
     }
 
     // Delete new rows from the table or archive/activate existing rows
     table.prototype.deleteRow = function (row) {
+        var self = this;
         row = $(row);
         if (row.data('isnew')) {
             // Its a new row so remove it (it does not need to be posted back because it never existed)
             row.next('.validation-row').remove();
             row.remove();
         } else {
-            var inputs = row.find('.table-control');
+            var inputs = row.find('input');
+            var checkboxes = row.find('input[type="checkbox"]');
             var isActiveInput = undefined;
-            var index = $('.edit-row').index(row);
+            var isDirtyInput = undefined;
             if (this.isActiveProperty !== undefined) {
-                isActiveInput = row.find('.' + this.isActiveProperty);
+                // Find the corresponding input
+                var attr = '[name="' + this.fieldName + '[' + row.index() + '].' + this.isActiveProperty + '"]';
+                isActiveInput = inputs.filter(attr);
+            }
+            if (this.isDirtyProperty !== undefined) {
+                // Find the corresponding input
+                var attr = '[name="' + this.fieldName + '[' + row.index() + '].' + this.isDirtyProperty + '"]';
+                isDirtyInput = inputs.filter(attr);
             }
             if (row.hasClass('archived')) {
                 // Its an existing row so mark it as active
                 row.removeClass('archived');
                 // Allow user interaction
-                $.each(inputs, function (index, item) {
-                    $(this).siblings('.table-text').remove();
-                    $(this).show();
-                });
+                inputs.prop('readonly', false);
+                checkboxes.off();
                 isActiveInput.val('True');
+
+                // TODO: Can use this because cant test hidden inputs (they dont have a defaultValue)!!
+                //isDirtyInput.val(this.isDirty(row) ? 'True' : 'False');
+
+                isDirtyInput.val('True');
                 // Raise event
                 $(this.element).trigger({
                     type: rowActivated,
-                    index: row.index() // TODO: which index taking into account validation rows?
+                    index: row.index()
                 });
             } else {
                 // Archive the row
                 row.addClass('archived');
-                // Prevent user interaction
-                $.each(inputs, function (index, item) {
-                    var value = $(this).val();
-                    if ($(this).is('select')) {
-                        value = $(this).children(':selected').text();
-                    } else if ($(this).is(':checkbox')) {
-                        value = $(this).is(':checked') ? 'Yes' : 'No';
-                    }
-                    var div = $('<div></div>').addClass('table-text').text(value);
-                    $(this).after(div);
-                    $(this).hide();
+                inputs.prop('readonly', true);
+                checkboxes.on('click', function () {
+                    return false;
                 });
                 isActiveInput.val('False');
+                isDirtyInput.val('True');
                 // Raise event
                 $(this.element).trigger({
                     type: rowDeleted,
-                    index: row.index() // TODO: which index taking into account validation rows?
+                    index: row.index()
                 });
             }
         }
@@ -254,18 +296,16 @@
     // Returns a value indicating if row values have changed from their original values
     table.prototype.isDirty = function (row) {
         var isDirty = false;
-        var inputs = $(row).find('.table-control');
+        var inputs = $(row).find('input');
         $.each(inputs, function () {
             var input = $(this);
+
+            var x = input.attr('name');
+
+            // TODO: What about the standard html select
             if (input.is(':checkbox') && input.prop('checked') !== input.prop('defaultChecked')) {
                 isDirty = true;
                 return false;
-            } else if (input.is('select')) {
-                var selected = input.children(':selected');
-                if (!selected.prop('defaultSelected')) {
-                    isDirty = true;
-                    return false;
-                }
             } else if (input.val() !== input.prop('defaultValue')) {
                 isDirty = true;
                 return false;
